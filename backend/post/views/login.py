c38538ae2afab99e35889a953cc07c2967c2b5b4
HoJun <sson514@naver.com>
2021-11-20 18:49:00 +0900

    2021-11-20 로그인, 메뉴 주문, 테이블 열람

--- backend/post/views/login.py
+++ backend/post/views/login.py
@@ -1,8 +1,4 @@
 import json
-from django.core.serializers.json import Serializer
-from django.db.models import fields
-from django.http import JsonResponse, HttpResponse
-from django.views import View
 from django.shortcuts import render
 from django.core.serializers import serialize
 from rest_framework import generics
@@ -13,26 +9,29 @@ from rest_framework.decorators import api_view
 
 @api_view(['POST'])
 def loginManager(request):
+    '''
+    매니저 로그인
+    '''
     try:
         data = json.loads(request.body)
-        print(type(data))
-        login_user = Staff.objects.filter(staff_id=data['id'])
-        print(login_user)
+        manager_id = "admin"
+        manager_pw = "1q2w3e4r"
 
-        if not login_user.exists() or login_user[0].staff_pw != data['password']:
-            return JsonResponse({'MESSAGE' : 'id 혹은 password가 다릅니다.'}, status=401)
+        if manager_id != data['id'] or manager_pw != data['password']:
+            return Response({'MESSAGE' : 'id 혹은 password가 다릅니다.'}, status=401)
 
     except KeyError:
-        return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+        return Response({'MESSAGE' : 'KEY_ERROR'}, status=400)
 
-    output_data = login_user.all()
+    output_data = {"id" : manager_id, "password" : manager_pw}
     print(output_data)
-    serialized_output_data = serializers.StaffSerializer(output_data, many=True)
-    print(serialized_output_data)
-    return Response(serialized_output_data.data, status=200)
+    return Response(output_data, status=200)
 
 @api_view(['POST'])
 def loginStaff(request):
+    '''
+    직원 로그인
+    '''
     try:
         data = json.loads(request.body)
         print(type(data))
@@ -40,10 +39,10 @@ def loginStaff(request):
         print(login_user)
 
         if not login_user.exists() or login_user[0].staff_pw != data['staff_pw']:
-            return JsonResponse({'MESSAGE' : 'id 혹은 password가 다릅니다.'}, status=401)
+            return Response({'MESSAGE' : 'id 혹은 password가 다릅니다.'}, status=401)
 
     except KeyError:
-        return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+        return Response({'MESSAGE' : 'KEY_ERROR'}, status=400)
 
     output_data = login_user.all()
     print(output_data)
@@ -53,17 +52,20 @@ def loginStaff(request):
 
 @api_view(['POST'])
 def findPW(request):
+    '''
+    비밀번호 찾기
+    '''
     try:
         input_data = json.loads(request.body)
         print(input_data)
         pw_user = Staff.objects.filter(staff_id=input_data['staff_id'])
 
         if not pw_user.exists():
-            return JsonResponse({'MESSAGE' : '존재하지 않는 id입니다.'}, status=401)
+            return Response({'MESSAGE' : '존재하지 않는 id입니다.'}, status=401)
         elif pw_user[0].name != input_data['name']:
-            return JsonResponse({'MESSAGE' : '존재하지 않는 이름입니다.'}, status = 402)
+            return Response({'MESSAGE' : '존재하지 않는 이름입니다.'}, status = 402)
         elif pw_user[0].phone_num != input_data['phone_num']:
-            return JsonResponse({'MESSAGE' : '존재하지 않는 전화번호입니다.'}, status = 403)
+            return Response({'MESSAGE' : '존재하지 않는 전화번호입니다.'}, status = 403)
         
         #output_data = serializers.StaffEncoder.serialize(pw_user)
         #print(output_data)
@@ -75,7 +77,7 @@ def findPW(request):
         
 
     except KeyError:
-        return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+        return Response({'MESSAGE' : 'KEY_ERROR'}, status=400)
 
     #return JsonResponse(output_data, safe = False, status = 200)
     return Response(serialized_output_data.data, status=200)
